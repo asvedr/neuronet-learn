94894ef03305d58a25cb334822e6053c0759c1c6
Arseny <asvedr@gmail.com>
2017-06-06 12:35:27 +0300

    full predictor

--- iris.py
+++ iris.py
@@ -8,6 +8,8 @@ class Neuron:
 	def predict(self, data):
 		assert len(self.weights) == len(data)
 		return (self.weights * data).sum() >= 0
+	def predict_num(self, data):
+		return (self.weights * data).sum()
 	def fit(self, X, rate, Y):
 		# fit rule
 		# w[j] = w[j] + Dw[j] where Dw[j] = n(y(i) - _y(i))x(i)[j]
@@ -22,6 +24,50 @@ class Neuron:
 		for j in range(len(self.weights)):
 			Dw = (Y - _Y) * X[j] * rate
 			self.weights[j] = self.weights[j] + Dw
+
+class Stat:
+	def __init__(self):
+		self.bad_yes = 0
+		self.bad_yes_log = []
+		self.bad_no  = 0
+		self.bad_no_log = []
+		self.total   = 0
+	def __str__(self):
+		return str({'bad_yes': self.bad_yes, 'bad_no': self.bad_no, 'total': self.total})
+	def __repr__(self):
+		return self.__str__()
+
+class Predictor:
+	def __init__(self):
+		self.neurons = [
+				('Iris-setosa', Neuron(4)),
+				('Iris-versicolor', Neuron(4)),
+				('Iris-virginica', Neuron(4))
+			]
+	def fit(self, data_set, set_name, rate):
+		for neuron_name,neuron in self.neurons:
+			neuron.fit(data_set, rate, set_name == neuron_name)
+	def predict(self, data):
+		for name,neuron in self.neurons:
+			if neuron.predict(data):
+				return name
+	def statistics(self, dataset):
+		stat = [(name,neur,Stat()) for name,neur in self.neurons]
+		for set in dataset:
+			X = set[:4]
+			Y = set[4]
+			for name,neuron,stobj in stat:
+				pred = neuron.predict(X)
+				if name == Y:
+					if not pred:
+						stobj.bad_no += 1
+						stobj.bad_no_log.append(neuron.predict_num(X))
+				else:
+					if pred:
+						stobj.bad_yes += 1
+						stobj.bad_yes_log.append(neuron.predict_num(X))
+				stobj.total += 1
+		return [(obj[0], obj[2]) for obj in stat]
 
 def get_set():
 	def read(t):
@@ -41,28 +87,28 @@ def split_by_percent(list, percent_in_first):
 		del set[i]
 	return (first, set)
 
-neuron = Neuron(4)
+predictor = Predictor()
 (fit, check) = split_by_percent(get_set(), 0.5)
-name = 'Iris-setosa'
 for _ in range(10):
 	for seq in fit:
-		neuron.fit(seq[:4], 1, seq[4] == name)
+		predictor.fit(seq[:4], seq[4], 1)
 fit_ok   = 0
 check_ok = 0
-print('FIT')
+# print('FIT')
 for seq in fit:
-	pred = neuron.predict(seq[:4])
-	ok = (seq[4] == name)
-	if pred == ok:
+	pred = predictor.predict(seq[:4])
+	if pred == seq[4]:
 		fit_ok += 1
-	print('X: %s, ANSWER: %s' % (seq, pred))
-print('CHECK')
+	# print('X: %s, ANSWER: %s' % (seq, pred))
+# print('CHECK')
 for seq in check:
-	pred = neuron.predict(seq[:4])
-	ok = seq[4] == name
-	if pred == ok:
+	pred = predictor.predict(seq[:4])
+	if pred == seq[4]:
 		check_ok += 1
-	print('X: %s, ANSWER: %s' % (seq, pred))
-print('on fit %s of %s' % (fit_ok, len(fit)))
-print('on check %s of %s' % (check_ok, len(check)))
-# test
+	# print('X: %s, ANSWER: %s' % (seq, pred))
+print('on fit %s of %s (%s%%)' % (fit_ok, len(fit), float(fit_ok) / len(fit)))
+print('on check %s of %s (%s%%)' % (check_ok, len(check), float(check_ok) / len(check)))
+for stat in predictor.statistics(fit + check):
+	print(stat)
+	# print('BY', stat[1].bad_yes_log)
+	# print('BN', stat[1].bad_no_log)
